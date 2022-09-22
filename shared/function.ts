2b28c2802d40be4d39941795e5e43c1797937562
kimelijahsol <kimelijahsol@gmail.com>
2022-09-22 22:22:58 +0900

    feat : duplicate function

---
+++ shared/function.ts
@@ -0,0 +1,35 @@
+interface GetDuplicatesAcc<T> {
+  value: T
+  count: number
+}
+
+/**
+ * 중복된 데이터들을 반환하는 함수
+ */
+export function getDuplicates<T>(array: T[]): T[] {
+  return array
+    .reduce((acc: GetDuplicatesAcc<T>[], current: T) => {
+      if (acc.some(({ value }) => value === current)) {
+        return acc.map((item) =>
+          item.value === current
+            ? {
+                ...item,
+                count: item.count + 1,
+              }
+            : item
+        )
+      } else {
+        return acc.concat({
+          value: current,
+          count: 1,
+        })
+      }
+    }, [])
+    .reduce((acc: T[], current: GetDuplicatesAcc<T>) => {
+      if (current.count > 1) {
+        return acc.concat(current.value)
+      }
+
+      return acc
+    }, [])
+}
