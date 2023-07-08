4900f7bcc3174f3e8a9e307a25bc83f6e09d595f
kim-elijah-sol <kimelijahsol@gmail.com>
2023-07-08 18:47:17 +0900

    feat: favicon

--- components/Seo.tsx
+++ components/Seo.tsx
@@ -4,7 +4,10 @@ function Seo({ children }: { children?: React.ReactNode }) {
   return (
     <Head>
       <title>Solog</title>
-      <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      <meta
+        name='viewport'
+        content='width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, viewport-fit=cover'
+      />
       <meta property='og:title' content='Solog' />
       <meta property='og:type' content='website' />
       <meta property='og:url' content='solog.io' />
