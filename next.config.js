648856f530d5edde9c723075fd2db624cf2d86d1
kim-elijah-sol <kimelijahsol@gmail.com>
2023-11-30 16:29:07 +0900

    feat: 스크롤 위치 복구

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const path = require('path')
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  experimental: {
+    scrollRestoration: true,
+  },
   images: {
     domains: ['newevolutiondesigns.com'],
   },
