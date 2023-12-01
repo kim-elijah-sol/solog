f19999f150a18de4da3d04ca4337d6191235d0d1
kim-elijah-sol <kimelijahsol@gmail.com>
2023-12-01 18:28:30 +0900

    feat: ContentCard 보여질 때 처리

--- components/ContentCard.tsx
+++ components/ContentCard.tsx
@@ -1,7 +1,10 @@
 import { css, useTheme } from '@emotion/react'
+import { motion } from 'framer-motion'
+import useIsMount from '@hooks/global/useIsMount'
 import { Content } from '@shared/contents'
 import { staticColor } from '@styles/palette'
 import Link from 'next/link'
+import { useEffect, useRef, useState } from 'react'
 import Flex from './layout/Flex'
 
 interface Props extends Content {
@@ -16,7 +19,34 @@ function ContentCard({
   description,
   createdAt,
 }: Props) {
+  const $card = useRef<HTMLDivElement>(null)
+
   const { color } = useTheme()
+
+  const isMount = useIsMount()
+
+  const [isView, setIsView] = useState(false)
+
+  useEffect(() => {
+    if (isMount && $card.current) {
+      const intersectionObserver = new IntersectionObserver(
+        (entries, observer) => {
+          const thisEntry = entries[0]
+
+          if (thisEntry.isIntersecting) {
+            setIsView(true)
+            observer.unobserve(thisEntry.target)
+          }
+        }
+      )
+
+      intersectionObserver.observe($card.current)
+
+      return () => {
+        intersectionObserver.disconnect()
+      }
+    }
+  }, [isMount])
 
   return (
     <Link
@@ -38,79 +68,86 @@ function ContentCard({
         },
       }}
     >
-      <Flex
-        css={{
-          alignItems: 'center',
-          '@media (max-width: 680px)': {
-            flexDirection: 'column',
-            alignItems: 'flex-start',
-          },
-        }}
+      <motion.div
+        initial={{ opacity: 0, translateY: 60 }}
+        animate={isView ? { opacity: 1, translateY: 0 } : undefined}
+        transition={{ duration: 0.4 }}
       >
-        <img
-          src={coverUrl}
+        <Flex
+          ref={$card}
           css={{
-            width: 180,
-            height: 180,
-            objectFit: 'cover',
-            borderRadius: '2rem',
-            marginRight: '2rem',
-            transition: 'transform 0.2s',
+            alignItems: 'center',
             '@media (max-width: 680px)': {
-              width: '100%',
-              marginRight: 0,
-              marginBottom: '1.75rem',
+              flexDirection: 'column',
+              alignItems: 'flex-start',
             },
-            border:
-              color.type === 'dark'
-                ? `1px solid rgba(255, 255, 255, 0.18)`
-                : `1px solid rgba(100, 100, 100, 0.18)`,
           }}
-        />
-
-        <div>
-          <h2
+        >
+          <img
+            src={coverUrl}
             css={{
-              fontSize: '2.25rem',
-              marginBottom: '2rem',
-              transition: 'color 0.2s',
-              wordBreak: 'keep-all',
-              lineHeight: '2.5rem',
+              width: 180,
+              height: 180,
+              objectFit: 'cover',
+              borderRadius: '2rem',
+              marginRight: '2rem',
+              transition: 'transform 0.2s',
               '@media (max-width: 680px)': {
-                fontSize: '1.75rem',
-                marginBottom: '1.25rem',
+                width: '100%',
+                marginRight: 0,
+                marginBottom: '1.75rem',
               },
+              border:
+                color.type === 'dark'
+                  ? `1px solid rgba(255, 255, 255, 0.18)`
+                  : `1px solid rgba(100, 100, 100, 0.18)`,
             }}
-          >
-            {title}
-          </h2>
+          />
 
-          <p
-            css={{
-              color: color.text_700,
-              fontSize: '1.15rem',
-              marginBottom: '1.75rem',
-              wordBreak: 'keep-all',
-              lineHeight: '1.5rem',
-              '@media (max-width: 680px)': {
+          <div>
+            <h2
+              css={{
+                fontSize: '2.25rem',
+                marginBottom: '2rem',
+                transition: 'color 0.2s',
+                wordBreak: 'keep-all',
+                lineHeight: '2.5rem',
+                '@media (max-width: 680px)': {
+                  fontSize: '1.75rem',
+                  marginBottom: '1.25rem',
+                },
+              }}
+            >
+              {title}
+            </h2>
+
+            <p
+              css={{
+                color: color.text_700,
+                fontSize: '1.15rem',
+                marginBottom: '1.75rem',
+                wordBreak: 'keep-all',
+                lineHeight: '1.5rem',
+                '@media (max-width: 680px)': {
+                  fontSize: '1rem',
+                  marginBottom: '1.25rem',
+                },
+              }}
+            >
+              {description}
+            </p>
+
+            <time
+              css={{
+                color: color.text_500,
                 fontSize: '1rem',
-                marginBottom: '1.25rem',
-              },
-            }}
-          >
-            {description}
-          </p>
-
-          <time
-            css={{
-              color: color.text_500,
-              fontSize: '1rem',
-            }}
-          >
-            {createdAt}
-          </time>
-        </div>
-      </Flex>
+              }}
+            >
+              {createdAt}
+            </time>
+          </div>
+        </Flex>
+      </motion.div>
     </Link>
   )
 }
