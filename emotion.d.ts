2c9b67b17f4b013020416adfb61b391736f84d20
kim-elijah-sol <kimelijahsol@gmail.com>
2023-12-01 11:45:54 +0900

    feat: emotion Theme > codeFontFamily 타입 추가

--- emotion.d.ts
+++ emotion.d.ts
@@ -1,11 +1,13 @@
 import '@emotion/react'
 import { ThemeType } from '@atoms/global/theme'
 import { lightColor, darkColor } from '@styles/palette'
+import { CodeFontFamilyType } from '@atoms/global/codeFontFamily'
 
 type ThemeColor = typeof lightColor | typeof darkColor
 
 declare module '@emotion/react' {
   export interface Theme {
     color: ThemeColor
+    codeFontFamily: CodeFontFamilyType
   }
 }
